75147ff78941a22c0c09d982e16578e6b0bc8876
leeyuhyun0104 <dldbgus0104@gmail.com>
2023-11-14 15:00:22 +0900

    article/write, step 1

--- src/main/java/com/ll/sb20231114/ArticleController.java
+++ src/main/java/com/ll/sb20231114/ArticleController.java
@@ -1,4 +1,13 @@
 package com.ll.sb20231114;
 
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class ArticleController {
+    @GetMapping("/article/write")
+    String showWrite() {
+        return "article/write";
+    }
 }
